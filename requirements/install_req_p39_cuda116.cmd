44adcecf5c2be075627d0620b542abb82ebceeb3
rathaROG <siv.ratha89@gmail.com>
2023-02-19 14:38:43 +0100

    Update requirements

--- requirements/install_req_p39_cuda116.cmd
+++ requirements/install_req_p39_cuda116.cmd
@@ -20,7 +20,7 @@ setlocal
 cd /d %~dp0
 python -m pip install --upgrade pip
 pip install --upgrade --force-reinstall setuptools
-pip install numpy>=1.24.2
+pip install "numpy>=1.24.2"
 pip install https://github.com/rathaumons/pyppbox-custpkg/raw/main/py39/cu116/opencv_contrib_python-4.7.0+cu116-cp39-cp39-win_amd64.whl
 pip install -r pippackages.txt
 pip install torch==1.13.1+cu116 torchaudio==0.13.1+cu116 torchvision==0.14.1+cu116 --extra-index-url https://download.pytorch.org/whl/cu116
