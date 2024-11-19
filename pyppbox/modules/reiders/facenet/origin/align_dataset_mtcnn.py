eb39814121a25eeda962774a3e1af599d5d4d795
Ratha SIV <60795657+rathaumons@users.noreply.github.com>
2024-11-19 14:33:22 +0100

    Remove unnecessary info from alignment log file of FaceNet

--- pyppbox/modules/reiders/facenet/origin/align_dataset_mtcnn.py
+++ pyppbox/modules/reiders/facenet/origin/align_dataset_mtcnn.py
@@ -48,8 +48,8 @@ def main(args):
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
     # Store some git revision info in a text file in the log directory
-    src_path,_ = os.path.split(os.path.realpath(__file__))
-    facenet.store_revision_info(src_path, output_dir, ' '.join(sys.argv))
+    # src_path,_ = os.path.split(os.path.realpath(__file__))
+    # facenet.store_revision_info(src_path, output_dir, ' '.join(sys.argv))
     dataset = facenet.get_dataset(args.input_dir)
     
     print('Creating networks and loading parameters')
