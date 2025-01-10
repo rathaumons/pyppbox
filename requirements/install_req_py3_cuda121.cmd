349c64cf34be1ab6bb2f810b8cb8b48d94b6e82a
rathaROG <siv.ratha89@gmail.com>
2025-01-10 14:48:32 +0100

    Remove `torchaudio` from install_req_py3_cuda121.cmd

--- requirements/install_req_py3_cuda121.cmd
+++ requirements/install_req_py3_cuda121.cmd
@@ -24,7 +24,7 @@ pip install "setuptools>=67.2.0"
 :: Uninstall conflict packages
 pip uninstall -y ultralytics
 :: Install common packages
-pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
+pip install torch torchvision --index-url https://download.pytorch.org/whl/cu121
 pip install -r requirements.txt
 cls
 :: Make sure there is no conflict
